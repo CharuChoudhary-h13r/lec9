5a237a83bbeae735164f4fa9140ef7f5e1bc3251
CharuChoudhary-h13r <charuchoudhary1201@gmail.com>
2026-07-03 00:45:09 +0530

    welcome in python

---
+++ break_statement_1to100.py
@@ -0,0 +1,13 @@
+# Q. print all prime numbers btw 1 to 100
+num=2
+while num<=100:
+    i=2
+    while i<num:
+        if num % i==0:
+            break
+
+        i+=1
+    if i==num:
+        print(num, end=" ")
+    num+=1
+            
